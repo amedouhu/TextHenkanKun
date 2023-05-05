8b5a4d703e6ccad2c31f39baa0df0c49439d638c
BuildTools <unconfigured@null.spigotmc.org>
2023-05-05 14:23:34 +0900

    1.0.0.4

---
+++ テキスト変換くん/テキスト変換くん.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace テキスト変換くん
+{
+    partial class テキスト変換くん
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(テキスト変換くん));
+            this.btEnter = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.plInput = new System.Windows.Forms.Panel();
+            this.lbInput = new System.Windows.Forms.Label();
+            this.plOutput = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.初期化ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plInput.SuspendLayout();
+            this.plOutput.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btEnter
+            // 
+            this.btEnter.BackColor = System.Drawing.Color.Red;
+            this.btEnter.FlatAppearance.BorderSize = 0;
+            this.btEnter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btEnter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btEnter.ForeColor = System.Drawing.Color.White;
+            this.btEnter.Location = new System.Drawing.Point(6, 133);
+            this.btEnter.Name = "btEnter";
+            this.btEnter.Size = new System.Drawing.Size(266, 23);
+            this.btEnter.TabIndex = 5;
+            this.btEnter.Text = "実行";
+            this.btEnter.UseVisualStyleBackColor = false;
+            this.btEnter.Click += new System.EventHandler(this.btEnter_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.BackColor = System.Drawing.Color.White;
+            this.tbInput.Location = new System.Drawing.Point(6, 32);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbInput.Size = new System.Drawing.Size(130, 95);
+            this.tbInput.TabIndex = 1;
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.BackColor = System.Drawing.Color.White;
+            this.tbOutput.Location = new System.Drawing.Point(142, 32);
+            this.tbOutput.MaxLength = 0;
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOutput.Size = new System.Drawing.Size(130, 95);
+            this.tbOutput.TabIndex = 3;
+            // 
+            // plInput
+            // 
+            this.plInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.plInput.Controls.Add(this.lbInput);
+            this.plInput.Location = new System.Drawing.Point(9, 32);
+            this.plInput.Name = "plInput";
+            this.plInput.Size = new System.Drawing.Size(109, 0);
+            this.plInput.TabIndex = 2;
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Location = new System.Drawing.Point(3, 2);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(29, 12);
+            this.lbInput.TabIndex = 0;
+            this.lbInput.Text = "入力";
+            // 
+            // plOutput
+            // 
+            this.plOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.plOutput.Controls.Add(this.label1);
+            this.plOutput.Location = new System.Drawing.Point(147, 32);
+            this.plOutput.Name = "plOutput";
+            this.plOutput.Size = new System.Drawing.Size(109, 0);
+            this.plOutput.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "出力";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.White;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem,
+            this.保存ToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(54, 22);
+            this.toolStripDropDownButton1.Text = "ファイル";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.開くToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.初期化ToolStripMenuItem});
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(44, 22);
+            this.toolStripDropDownButton2.Text = "編集";
+            // 
+            // 初期化ToolStripMenuItem
+            // 
+            this.初期化ToolStripMenuItem.Name = "初期化ToolStripMenuItem";
+            this.初期化ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.初期化ToolStripMenuItem.Text = "初期化";
+            this.初期化ToolStripMenuItem.Click += new System.EventHandler(this.初期化ToolStripMenuItem_Click);
+            // 
+            // テキスト変換くん
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.plOutput);
+            this.Controls.Add(this.plInput);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.btEnter);
+            this.Controls.Add(this.tbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "テキスト変換くん";
+            this.Text = "テキスト変換くん";
+            this.plInput.ResumeLayout(false);
+            this.plInput.PerformLayout();
+            this.plOutput.ResumeLayout(false);
+            this.plOutput.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btEnter;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Panel plInput;
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.Panel plOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem 初期化ToolStripMenuItem;
+    }
+}
+
---
+++ テキスト変換くん/テキスト変換くん.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace テキスト変換くん
+{
+    public partial class テキスト変換くん : Form
+    {
+        public テキスト変換くん()
+        {
+            InitializeComponent();
+        }
+
+        private void btEnter_Click(object sender, EventArgs e)
+        {
+            // 実行ボタンがクリックされたとき
+            string input = tbInput.Text;
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            tbOutput.Text = null;
+            foreach (byte b in bytes) {
+                tbOutput.AppendText(Convert.ToString(b, 2).PadLeft(8, '0') + Environment.NewLine);
+            }
+        }
+
+        private void 開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 開くボタンがクリックされたとき
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Title = "開く";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string text = File.ReadAllText(filePath);
+                tbInput.Text = text;
+            }
+            openFileDialog.Dispose();
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 保存ボタンがクリックされたとき
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "テキストファイル|*.txt";
+            saveFileDialog.FileName = "テキスト変換くん.txt";
+            saveFileDialog.Title = "保存";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                File.WriteAllText(saveFileDialog.FileName, tbOutput.Text);
+            }
+
+        }
+
+        private void 初期化ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 初期化ボタンがクリックされたとき
+            tbInput.Text = null;
+            tbOutput.Text = null;
+        }
+    }
+}
